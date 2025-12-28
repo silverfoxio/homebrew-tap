50780e799e5aafb0c9bdd0685b7b84dbb63e837c
silverfoxdev <sushant.lokhande2010@gmail.com>
2025-12-28 18:54:05 +0000

    initial commit

---
+++ Formula/gitdrift.rb
@@ -0,0 +1,34 @@
+class GitDrift < Formula
+  desc "CLI tool to detect missing Git commits across branches"
+  homepage "https://github.com/silverfoxio/gitdrift"
+  version "0.1.0"
+  license "MIT OR Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/silverfoxio/gitdrift/releases/download/v0.1.0/gitdrift-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "ARM_MAC_SHA256"
+    else
+      url "https://github.com/silverfoxio/gitdrift/releases/download/v0.1.0/gitdrift-v0.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "INTEL_MAC_SHA256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/silverfoxio/gitdrift/releases/download/v0.1.0/gitdrift-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ARM_LINUX_SHA256"
+    else
+      url "https://github.com/silverfoxio/gitdrift/releases/download/v0.1.0/gitdrift-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "INTEL_LINUX_SHA256"
+    end
+  end
+
+  def install
+    bin.install "gitdrift"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gitdrift --version")
+  end
+end
